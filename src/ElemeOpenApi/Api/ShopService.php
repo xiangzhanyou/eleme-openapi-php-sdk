4f13bbc6870af14776d0d2af5fb15ca7c463e061
唐雁 <yan.tang02@ele.me>
2017-03-02 16:30:01 +0800

    add info

--- src/ElemeOpenApi/Api/ShopService.php
+++ src/ElemeOpenApi/Api/ShopService.php
@@ -5,14 +5,23 @@ namespace ElemeOpenApi\Api;
 class ShopService extends RpcService
 {
 
+    /** 获取店铺信息
+     * @param int $shop_id 店铺Id
+     * @return mixed
+     */
     public function get_shop($shop_id)
     {
         return $this->client->call("eleme.shop.getShop", array("shopId" => $shop_id));
     }
 
-    public function update_shop($shop_id, $properties)
+    /** 更新店铺信息
+     * @param int $shop_id 店铺Id
+     * @param mixed $property 店铺更新属性
+     * @return mixed
+     */
+    public function update_shop($shop_id, $property)
     {
-        return $this->client->call("eleme.shop.updateShop", array("shopId" => $shop_id, "properties" => $properties));
+        return $this->client->call("eleme.shop.updateShop", array("shopId" => $shop_id, "property" => $property));
     }
 
 }
--- src/ElemeOpenApi/Api/UserService.php
+++ src/ElemeOpenApi/Api/UserService.php
@@ -5,6 +5,10 @@ namespace ElemeOpenApi\Api;
 class UserService extends RpcService
 {
 
+    /** 获取用户
+    
+     * @return mixed
+     */
     public function get_user()
     {
         return $this->client->call("eleme.user.getUser", array());
